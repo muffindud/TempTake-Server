2c70b5371f6f68b1aa5f7225f4a230d3d335c564
muffindud <ccatlabuga2002@gmail.com>
2025-05-21 14:37:25 +0300

    add internal auth

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using TempTake_Server.Context;
 using TempTake_Server.Interfaces;
 using TempTake_Server.Repository;
 
 
+var internalSecret = Environment.GetEnvironmentVariable("INTERNAL_SECRET") ?? "default_secret";
 var serverPort = int.Parse(Environment.GetEnvironmentVariable("SERVER_PORT") ?? "8080");
 var dbServer = Environment.GetEnvironmentVariable("DB_HOST") ?? "localhost";
 var dbName = Environment.GetEnvironmentVariable("DB_NAME") ?? "temptake";
@@ -12,7 +17,25 @@ var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD") ?? "postgres"
 var connectionString = $"Server={dbServer};Database={dbName};User Id={dbUser};Password={dbPassword};";
 
 var builder = WebApplication.CreateBuilder(args);
+var requireAuthPolicy = new AuthorizationPolicyBuilder()
+    .RequireAuthenticatedUser()
+    .Build();
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(jwtOptions =>
+    {
+        jwtOptions.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(internalSecret)),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero
+        };
+    });
+builder.Services.AddAuthorizationBuilder()
+    .SetDefaultPolicy(requireAuthPolicy);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.WebHost.UseUrls($"http://*:{serverPort}");
